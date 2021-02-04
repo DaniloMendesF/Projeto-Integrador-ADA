c75e908fc4642d86fca2a51633805ba44a5d6211
jackakemi <jackeline.akemii@gmail.com>
2021-02-04 11:54:19 -0300

    Adição da camada de Security & Service

---
+++ ADA/src/main/java/com/ada/ADA/model/UserLogin.java
@@ -0,0 +1,79 @@
+package com.ada.ADA.model;
+
+public class UserLogin {
+	
+	private String nomeCompleto;
+	
+	private String email;
+	
+	private String usuario;
+	
+	private String cargo;
+	
+	private String senha;
+	
+	private String link;	
+
+	private String token;
+
+	
+
+	//Início Getters/Setters
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+	
+	public String getLink() {
+		return link;
+	}
+
+	public void setLink(String link) {
+		this.link = link;
+	}
+	
+	public String getToken() {
+		return token;
+	}
+
+	public void setToken(String token) {
+		this.token = token;
+	}
+	
+	
+}
